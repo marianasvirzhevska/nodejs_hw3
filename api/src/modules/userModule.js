c8ded04972d8cca906769b882a0051fec1395263
Mariana Svirzhevska <mariana_svirzhevska@epam.com>
2020-03-28 14:25:42 +0200

    Bugfix

--- api/src/modules/userModule.js
+++ api/src/modules/userModule.js
@@ -13,20 +13,18 @@ const userSchema = mongoose.Schema({
     },
     role: String,
     password: String,
-    password_repeat: String,
 });
 
 const UserModel = mongoose.model('User', userSchema);
 
 const userValidateSchema = Joi.object({
-    firstName: Joi.string().min(3).max(30).required(),
-    lastName: Joi.string().min(3).max(30).required(),
+    firstName: Joi.string().min(2).max(30).required(),
+    lastName: Joi.string().min(2).max(30).required(),
     email: Joi.string().email().required(),
     phone: Joi.string().max(12).required(),
     created: Joi.date().max('1-1-2050').iso(),
     role: Joi.string().required(),
-    password: Joi.string().pattern(/^[a-zA-Z0-9]{6,10}$/).required(),
-    password_repeat: Joi.ref('password'),
+    password: Joi.string().pattern(/^[a-zA-Z0-9]{8,30}$/).required(),
 });
 
 module.exports = {
