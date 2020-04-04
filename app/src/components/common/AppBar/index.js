0356b48ac673e5e4888d837eb98038fd9d4d4794
Mariana Svirzhevska <mariana_svirzhevska@epam.com>
2020-04-04 15:08:41 +0300

    Add logout

--- app/src/components/common/AppBar/index.js
+++ app/src/components/common/AppBar/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
 import styles from './AppBar.module.sass';
+import { LOGOUT } from '../../../store/constants';
+
 
 const AppBar = ( props ) => {
     const { title } = props;
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const logout = () => {
+        dispatch({ type: LOGOUT });
+        localStorage.removeItem('user');
+        history.push('/');
+    };
 
     return (
         <div className={styles.appBar}>
-            <h1 className={styles.title}>
-                {title}
-            </h1>
+            <div className="container">
+                <div className="container-fluid">
+                    <div className={styles.appContainer}>
+                        <h1 className={styles.title}>
+                            {title}
+                        </h1>
+                        <div className={styles.logoutBtn}>
+                            <Button
+                                variant="outlined"
+                                size="small"
+                                color="primary"
+                                onClick={logout}
+                            >Logout</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
